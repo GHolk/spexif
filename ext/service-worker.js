fb92724a051f92eda58cad4ee590771c7a22b6ab
gholk <85619andy@gmail.com>
2018-01-08 17:02:28 +0800

    remove async event handler

--- ext/service-worker.js
+++ ext/service-worker.js
@@ -1,51 +1,59 @@
 
 const version = 'v2'
 
-self.addEventListener('install', async (installEvent) => {
-    const cache = await caches.open(version)
-    const cacheAll = cache.addAll([
-        './',
-        'index.html',
-        'doc/report.html',
-        'ext/cacheImage.js',
-        'ext/domHelper.js',
-        'ext/errorSpeaker.js',
-        'ext/filterPiexif.js',
-        'ext/googleMapWraper.js',
-        'ext/icon144.png',
-        'ext/icon48.png',
-        'ext/imageList.js',
-        'ext/inputFile.js',
-        'ext/leafletMapWraper.js',
-        'ext/manifest.json',
-        'ext/mobile.css',
-        'ext/piexif.js',
-        'ext/service-worker.js',
-        'ext/style.css'
-    ])
-    console.log('cache all file')
-    installEvent.waitUntil(cacheAll)
-})
-
-self.addEventListener('fetch', async (fetchEvent) => {
-    const cacheResponse = await caches.match(fetchEvent.request)
-    if (cacheResponse) {
-        console.log('match cache: %s', fetchEvent.request)
-        fetchEvent.respondWith(cacheResponse)
-    }
-    else {
-        console.log('no match cache: %s', fetchEvent.request)
-        const newResponse = await fetch(fetchEvent.request)
+self.addEventListener('install', (installEvent) => {
+    async function cacheAll() {
         const cache = await caches.open(version)
-        cache.put(fetchEvent.request, newResponse.clone())
-        fetchEvent.respondWith(newResponse)
+        return cache.addAll([
+            './',
+            'index.html',
+            'doc/report.html',
+            'ext/cacheImage.js',
+            'ext/domHelper.js',
+            'ext/errorSpeaker.js',
+            'ext/filterPiexif.js',
+            'ext/googleMapWraper.js',
+            'ext/icon144.png',
+            'ext/icon48.png',
+            'ext/imageList.js',
+            'ext/inputFile.js',
+            'ext/leafletMapWraper.js',
+            'ext/manifest.json',
+            'ext/mobile.css',
+            'ext/piexif.js',
+            'ext/service-worker.js',
+            'ext/style.css'
+        ])
     }
+    installEvent.waitUntil(cacheAll())
+    console.log('cache all file')
 })
 
-self.addEventListener('activate', async (activateEvent) => {
-    const keys = await caches.keys()
-    const deleteOld = keys
-          .filter((key) => key != version)
-          .map((key) => caches.delete(key))
-    activateEvent.waitUntil(Promise.all(deleteOld))
+self.addEventListener('fetch', (fetchEvent) => {
+    async function getResponse() {
+        const cacheResponse = await caches.match(fetchEvent.request)
+        if (cacheResponse) {
+            console.log('match cache: %s', fetchEvent.request)
+            return cacheResponse
+        }
+        else {
+            console.log('no match cache: %s', fetchEvent.request)
+            const newResponse = await fetch(fetchEvent.request)
+            const cache = await caches.open(version)
+            await cache.put(fetchEvent.request, newResponse.clone())
+            return newResponse
+        }
+    }
+    fetchEvent.respondWith(getResponse())
+})
+
+self.addEventListener('activate', (activateEvent) => {
+    async function clearCache() {
+        const keys = await caches.keys()
+        const deleteOld = keys
+            .filter((key) => key != version)
+            .map((key) => caches.delete(key))
+        return Promise.all(deleteOld)
+    }
+    activateEvent.waitUntil(clearCache())
 })
